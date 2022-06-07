f018d2efdaf10950a6f7bb342cec428b95c4a31d
bear-is-asleep <67704640+bear-is-asleep@users.noreply.github.com>
2022-06-07 14:53:19 -0500

    Updates

--- cuts_plots.py
+++ cuts_plots.py
@@ -20,9 +20,16 @@ data_dir = '/pnfs/sbnd/persistent/users/brindenc/analyze_sbnd/nue/v09_43_00/data
 #data_dir = 'data'
 pot1 = 6.6e20
 pot2 = 10e20
-SEED = 69420
-Etheta1 = 0.03 #Gev rad^2
-Etheta2 = 0.04 #Gev rad^2
+SEED = 42
+Etheta1 = 0.003 #Gev rad^2
+Etheta2 = 0.004 #Gev rad^2
+Etheta3 = 0.001
+cut1_str = r'$E_e \theta _e ^2 <$ 0.003 [GeV rad$^2$]'
+cut2_str = r'$E_e \theta _e ^2 <$ 0.004 [GeV rad$^2$]'
+cut3_str = r'$E_e \theta _e ^2 <$ 0.001 [GeV rad$^2$]'
+bw_e = 0.1
+bw_t = 0.02
+bw_et = 0.0005
 
 
 #Load data
@@ -39,8 +46,10 @@ nue_df,_ = nuepic.get_genie_df(f'{data_dir}/nue.root','NuEScat','Event')
 nuecc_df,_ = nuepic.get_genie_df(f'{data_dir}/nuecc.root','NuEScat','Event')
 
 #POT per event
-nue_pot_per_event = pot_nue/nue_df.index.drop_duplicates().shape[0]
-nuecc_pot_per_event = pot_nuecc/nuecc_df.index.drop_duplicates().shape[0]
+#nue_pot_per_event = pot_nue/nue_df.index.drop_duplicates().shape[0]
+#nuecc_pot_per_event = pot_nuecc/nuecc_df.index.drop_duplicates().shape[0]
+nue_pot_per_event = -1 #Temporary value to use raw pot info method
+nuecc_pot_per_event = -1
 
 
 
@@ -53,41 +62,129 @@ back_pot2,_ = nuepic.get_pot_normalized_df(nuecc,pot2,pot_nuecc,events_nuecc,see
 nue_pot2,_ = nuepic.get_pot_normalized_df(nue,pot2,pot_nue,events_nue,seed=SEED,pot_per_event=nue_pot_per_event)
 
 #Get s/b
-sb0 = nuepic.get_signal_background(nue_pot1,back_pot1)
+sb1 = nuepic.get_signal_background(nue_pot1,back_pot1)
+sb2 = nuepic.get_signal_background(nue_pot2,back_pot2)
 #sb0 = nuepic.get_signal_background(nue,nuecc)
 
 #Make precut plots
-ax,fig = nueplotters.hist_scatback(nue_pot1,back_pot1,'genie_Eng',sb0,alpha=0.6,
-          title=r'$e^-$ Energy Distribution 6.6 $\times$10$^{20}$ POT'+'\nPrecut',xlabel='Energy [GeV]',ylabel='')
-plotters.save_plot('E_pot1__precut',folder_name=energy_folder)
-ax,fig = nueplotters.hist_scatback(nue_pot1,back_pot1,'theta_t',sb0,alpha=0.6,
-          title=r'$\theta_e$ Distribution 6.6 $\times$10$^{20}$ POT'+'\n Precut',xlabel=r'$\theta _e$ [rad]',ylabel='',
-          nbins=100)
-ax.set_xlim([-0.1,0.5])
-plotters.save_plot('E_pot1__precut',folder_name=thetae_folder)
-ax,fig = nueplotters.hist_scatback(nue_pot1,back_pot1,'E_theta^2',sb0,alpha=0.6,
-          title=r'$E_e \theta_e^2$ Distribution 6.6 $\times$10$^{20}$ POT'+'\nPrecut',xlabel=r'$E_e \theta_e^2$ [GeV rad$^2$]',ylabel='')
-plotters.save_plot('Ethetae_pot1__precut',folder_name=cuts_folder)
+# ax,fig = nueplotters.hist_scatback(nue_pot1,back_pot1,'genie_Eng',sb0,alpha=0.6,
+#           title=r'$e^-$ Energy Distribution 6.6 $\times$10$^{20}$ POT'+'\nPrecut',xlabel='Energy [GeV]',
+#           ylabel='',bw=bw_e)
+# plotters.save_plot('E_pot1__precut',folder_name=energy_folder)
+# ax,fig = nueplotters.hist_scatback(nue_pot1,back_pot1,'theta_t',sb0,alpha=0.6,
+#           title=r'$\theta_e$ Distribution 6.6 $\times$10$^{20}$ POT'+'\n Precut',xlabel=r'$\theta _e$ [rad]',ylabel='',
+#           bw=bw_t)
+# plotters.save_plot('theta_pot1__precut',folder_name=thetae_folder)
+# ax.set_xlim([-0.01,0.5])
+# plotters.save_plot('theta_pot1__precut_zoom',folder_name=thetae_folder)
+# ax,fig = nueplotters.hist_scatback(nue_pot1,back_pot1,'E_theta^2',sb0,alpha=0.6,
+#           title=r'$E_e \theta_e^2$ Distribution 6.6 $\times$10$^{20}$ POT'+'\nPrecut',xlabel=r'$E_e \theta_e^2$ [GeV rad$^2$]',ylabel='',
+#           bw=0.1)
+# plotters.save_plot('Ethetae_pot1__precut',folder_name=cuts_folder)
+# ax,fig = nueplotters.hist_scatback(nue_pot1,back_pot1,'E_theta^2',sb0,alpha=0.6,
+#           title=r'$E_e \theta_e^2$ Distribution 6.6 $\times$10$^{20}$ POT'+'\nPrecut',xlabel=r'$E_e \theta_e^2$ [GeV rad$^2$]',ylabel='',
+#           bw=bw_et)
+# ax.set_xlim([0,0.05])
+# plotters.save_plot('Ethetae_pot1__precut_zoom',folder_name=cuts_folder)
 
 #Calc cuts
 back_pot1_cut1 = nuepic.make_cuts(back_pot1,Etheta=Etheta1)
 back_pot1_cut2 = nuepic.make_cuts(back_pot1,Etheta=Etheta2)
+back_pot1_cut3 = nuepic.make_cuts(back_pot1,Etheta=Etheta3)
 back_pot2_cut1 = nuepic.make_cuts(back_pot2,Etheta=Etheta1)
 back_pot2_cut2 = nuepic.make_cuts(back_pot2,Etheta=Etheta2)
+back_pot2_cut3 = nuepic.make_cuts(back_pot2,Etheta=Etheta3)
 
 nue_pot1_cut1 = nuepic.make_cuts(nue_pot1,Etheta=Etheta1)
 nue_pot1_cut2 = nuepic.make_cuts(nue_pot1,Etheta=Etheta2)
+nue_pot1_cut3 = nuepic.make_cuts(nue_pot1,Etheta=Etheta3)
 nue_pot2_cut1 = nuepic.make_cuts(nue_pot2,Etheta=Etheta1)
 nue_pot2_cut2 = nuepic.make_cuts(nue_pot2,Etheta=Etheta2)
+nue_pot2_cut3 = nuepic.make_cuts(nue_pot2,Etheta=Etheta3)
 
 #Get new s/b
 sb1_pot1_cut1 = nuepic.get_signal_background(nue_pot1_cut1,back_pot1_cut1)
 sb1_pot1_cut2 = nuepic.get_signal_background(nue_pot1_cut2,back_pot1_cut2)
+sb1_pot1_cut3 = nuepic.get_signal_background(nue_pot1_cut3,back_pot1_cut3)
 sb1_pot2_cut1 = nuepic.get_signal_background(nue_pot2_cut1,back_pot2_cut1)
 sb1_pot2_cut2 = nuepic.get_signal_background(nue_pot2_cut2,back_pot2_cut2)
+sb1_pot2_cut3 = nuepic.get_signal_background(nue_pot2_cut3,back_pot2_cut3)
 
-print(sb0,sb1_pot1_cut1,sb1_pot1_cut2)
+#Make arrays for data
+Ethetas = [Etheta1,Etheta2,Etheta3]
+pots = [pot1,pot2]
+potstrs = [r'6.6 $\times$10$^{20}$ POT',r'10$^{21}$ POT']
+sb_precuts = [sb1,sb2]
+sbs = [[sb1_pot1_cut1,sb1_pot2_cut1],[sb1_pot1_cut2,sb1_pot2_cut2],[sb1_pot1_cut3,sb1_pot2_cut3]]
+nues = [[nue_pot1_cut1,nue_pot2_cut1],[nue_pot1_cut2,nue_pot2_cut2],[nue_pot1_cut3,nue_pot2_cut3]]
+backs = [[back_pot1_cut1,back_pot2_cut1],[back_pot1_cut2,back_pot2_cut2],[back_pot1_cut3,back_pot2_cut3]]
+nue_precuts = [nue_pot1,nue_pot2]
+back_precuts = [back_pot1,back_pot2]
+
+#Make postcut plots
+for i in range(1):#3
+  for j in range(1):#2
+    sb_precut = sb_precuts[j]
+    sb = sbs[i][j]
+    nue = nues[i][j]
+    back = backs[i][j]
+    Etheta=Ethetas[i]
+    savestr = f'cut{i}_pot{j}'
+    potstr = potstrs[j]
+    nue_precut = nue_precuts[j]
+    back_precut = back_precuts[j]
+
+    #Precut plots
+    if i == 0:
+      ax,fig = nueplotters.hist_scatback(nue_precut,back_precut,'genie_Eng',sb_precut,alpha=0.6,
+                title=r'$E_e$ Distribution '+potstr+'\n'+'Precut',
+                xlabel='Energy [GeV]',ylabel='',bw=bw_e)
+      plotters.save_plot(f'E_pot{j}__precut',folder_name=energy_folder)
+      ax,fig = nueplotters.hist_scatback(nue_precut,back_precut,'theta_t',sb_precut,alpha=0.6,
+                title=r'$\theta_e$ Distribution '+potstr+'\nPrecut',
+                xlabel=r'$\theta _e$ [rad]',ylabel='',bw=bw_t)
+      plotters.save_plot(f'thetae_pot{j}__precut',folder_name=thetae_folder)
+      ax.set_xlim([-0.01,0.5])
+      plotters.save_plot(f'thetae_pot{j}__precut_zoom',folder_name=thetae_folder)
+      plt.close()
+      ax,fig = nueplotters.hist_scatback(nue_precut,back_precut,'E_theta^2',sb_precut,alpha=0.6,
+                title=r'$E_e \theta_e^2$ Distribution '+potstr+'\nPrecut',
+                xlabel=r'$E_e \theta_e^2$ [GeV rad$^2$]',ylabel='',
+                bw=0.1)
+      plotters.save_plot(f'Ethetae_pot{j}__precut',folder_name=cuts_folder)
+      plt.close()
+      ax,fig = nueplotters.hist_scatback(nue_precut,back_precut,'E_theta^2',sb_precut,alpha=0.6,
+                title=r'$E_e \theta_e^2$ Distribution '+potstr+'\nPrecut',
+                xlabel=r'$E_e \theta_e^2$ [GeV rad$^2$]',ylabel='',
+                bw=bw_et*2)
+      ax.set_xlim([0,0.015])
+      plotters.save_plot(f'Ethetae_pot{j}__precut_zoom',folder_name=cuts_folder)
+      plt.close()
+
+
+    #Postcut plots
+    ax,fig = nueplotters.hist_scatback(nue,back,'genie_Eng',sb,alpha=0.6,
+              title=r'$E_e$ Distribution '+potstr+'\n'+r'$E_e \theta_e^2 <$'+f'{Etheta}',
+              xlabel='Energy [GeV]',ylabel='',bw=bw_e)
+    plotters.save_plot(f'E_{savestr}',folder_name=energy_folder)
+    plt.close()
+    ax,fig = nueplotters.hist_scatback(nue,back,'theta_t',sb,alpha=0.6,
+              title=r'$\theta_e$ Distribution '+potstr+'\n'+r'$E_e \theta_e^2 <$'+f'{Etheta}',
+              xlabel=r'$\theta _e$ [rad]',ylabel='',bw=bw_t)
+    ax.set_xlim([-0.01,0.5])
+    plotters.save_plot(f'thetae_{savestr}',folder_name=thetae_folder)
+    plt.close()
+    ax,fig = nueplotters.hist_scatback(nue,back,'E_theta^2',sb,alpha=0.6,
+              title=r'$E_e \theta_e^2$ Distribution '+potstr+'\n'+r'$E_e \theta_e^2 <$'+f'{Etheta}',
+              xlabel=r'$E_e \theta_e^2$ [GeV rad$^2$]',ylabel='',
+              bw=bw_et)
+    ax.set_xlim([None,Etheta+bw_et])
+    plotters.save_plot(f'Ethetae_{savestr}',folder_name=cuts_folder)
+    plt.close()
+
+#print(sb0,sb1_pot1_cut1,sb1_pot1_cut2)
 
 #Make new plots here
+
 
 
--- preprocessing_nue.py
+++ preprocessing_nue.py
@@ -3,47 +3,87 @@ sys.path.append('/sbnd/app/users/brindenc/mypython') #My utils path
 from bc_utils.nueutils import pic as nuepic
 from bc_utils.utils import pic
 from time import time
+import numpy as np
+import pandas as pd
 
 #Constants/parameters
-E_threshold = 0.02 #GeV energy threshold for visible hadron
+E_threshold = 0.021 #GeV energy threshold for visible hadron (ArgoNeut)
+E_threshold_exotic = E_threshold #Set same for now
 data_dir = '/pnfs/sbnd/persistent/users/brindenc/analyze_sbnd/nue/v09_43_00/data'
 savename = 'nuecc.pkl'
+savename_Np = 'nueccNP.pkl'
 savename_signal = 'nue.pkl'
+exotic_hadrons = [3112,321,4122,3222] #Make cuts on these?
 
 t1 = time()
 
 #Load files
 nue_df,_ = nuepic.get_genie_df(f'{data_dir}/nue.root','NuEScat','Event')
 nuecc_df,_ = nuepic.get_genie_df(f'{data_dir}/nuecc.root','NuEScat','Event')
+#nuecc_df = pd.read_pickle('data/nuecc.pkl') #temp test
+
+#Sort indeces
+nue_df = nue_df.sort_index()
+nuecc_df = nuecc_df.sort_index()
+
 t2 = time()
 pic.print_stars()
 
 print(f'Load time {t2-t1:.2f} s')
 
 #Make nuecc cuts
-cut1 = nuepic.cut_pdg_event(nuecc_df,211,E_threshold=E_threshold) #Pions
-cut2 = nuepic.cut_pdg_event(cut1,2212,E_threshold=E_threshold) #Protons
+events_cut = np.zeros(len(exotic_hadrons)+5) #Display number of events cut for each step
+columns = ['Precut','nc','nu+e','pion','proton']
+columns.extend(['pdg '+str(x) for x in exotic_hadrons])
+events_cut[0] = nuecc_df.index.drop_duplicates().shape[0]
+
+cut1 = nuecc_df[nuecc_df['ccnc_truth'] == 0] #Keep only cc events
+events_cut[1] = cut1.index.drop_duplicates().shape[0]
+
+cut2 = nuepic.find_hadron_activity(cut1,drop=True) #Cuts for nu e events seen in background
+events_cut[2] = cut2.index.drop_duplicates().shape[0]
+
+cut3 = nuepic.cut_pdg_event(cut2,211,E_threshold=E_threshold) #Pions
+events_cut[3] = cut3.index.drop_duplicates().shape[0]
+
+cut4 = nuepic.cut_pdg_event(cut3,2212,E_threshold=E_threshold) #Protons
+events_cut[4] = cut4.index.drop_duplicates().shape[0]
+
+
+
 
 #Exotic particle cuts
-pdgs_seen = [3112,321,4122,3222] #Make cuts on these?
-cut3 = nuepic.find_hadron_activity(cut2,drop=True) #Cuts for nu e events seen in background
-cut4 = cut3[cut3['ccnc_truth'] == 0] #Keep only cc events
+cut5 = cut4.copy() #Set temporary df
+for i,pdg in enumerate(exotic_hadrons): #this will iteravily cut hadrons
+  temp_cut = nuepic.cut_pdg_event(cut5,pdg,E_threshold=E_threshold_exotic)
+  cut5 = temp_cut.copy()
+  events_cut[i+5] = cut5.index.drop_duplicates().shape[0]
 
+#Print events cut
 print(f'Precut events {nuecc_df.index.drop_duplicates().shape[0]}')
-print(f'Cut pion events {cut1.index.drop_duplicates().shape[0]}')
-print(f'Cut proton events {cut2.index.drop_duplicates().shape[0]}')
-print(f'Cut nu+e events {cut3.index.drop_duplicates().shape[0]}')
-print(f'Cut non cc events {cut4.index.drop_duplicates().shape[0]}')
+print(f'Cut non cc events {cut1.index.drop_duplicates().shape[0]}')
+print(f'Cut nu+e events {cut2.index.drop_duplicates().shape[0]}')
+print(f'Cut pion events {cut3.index.drop_duplicates().shape[0]}')
+print(f'Cut proton events (keep antiparticles) {cut4.index.drop_duplicates().shape[0]}')
+for i,pdg in enumerate(exotic_hadrons):
+  print(f'Cut pdg {pdg} events {events_cut[i+5]}')
+
+#Save cut information
+cuts_df = pd.DataFrame(events_cut,index=columns)
+#cuts_df.to_csv(f'{data_dir}/nuecc_cuts.csv')
+cuts_df.to_csv(f'data/nuecc_cuts.csv')
+
 
 t3 = time()
-print(f'Cut time {t3-t2:.2f} s')
+print(f'\nCut time {t3-t2:.2f} s')
 
 #Drop at rest electrons (initial state)
 nue_0 = nuepic.drop_initial_e(nue_df) #Get rid of initial electron in dataframe
 
 #Add electron count info, just in case 
 #nuecc_1 = nuepic.get_electron_count(cut4)
-nuecc_1 = nuepic.get_electron_count(cut3) #Include cc and nc events
+nuecc_1 = nuepic.get_electron_count(cut4) #Select cut stage
+nueccNp_1 = nuepic.get_electron_count(cut3) #Select cut stage
 nue_1 = nuepic.get_electron_count(nue_0)
 
 #Get background and scat types
@@ -52,18 +92,25 @@ nue_2 = nuepic.get_scat_type(nue_1)
 
 #Calc thetae and Etheta^2
 nuecc_2 = nuepic.calc_thetat(nuecc_1,method=1) #Use momentum method
-nuecc_3 = nuepic.calc_Etheta(nuecc_2)
+nueccNp_2 = nuepic.calc_thetat(nueccNp_1,method=1) #Use momentum method
+#nuecc_3 = nuepic.calc_thetave(nuecc_2,return_key='theta_ve')
+nuecc_4 = nuepic.calc_Etheta(nuecc_2)
+nueccNp_4 = nuepic.calc_Etheta(nueccNp_2)
+#nuecc_4.loc[:,'E_theta_ve'] = nuecc_4.loc[:,'genie_Eng']*nuecc_4.loc[:,'theta_ve']**2
 nue_3 = nuepic.calc_thetat(nue_2,method=1) #Use momentum method
-nue_4 = nuepic.calc_Etheta(nue_3)
+#nue_4 = nuepic.calc_thetave(nue_3,return_key='theta_ve')
+nue_5 = nuepic.calc_Etheta(nue_3)
+#nue_5.loc[:,'E_theta_ve'] = nue_5.loc[:,'genie_Eng']*nue_5.loc[:,'theta_ve']**2
 
 t4 = time()
 print(f'Calc and organize new vars. time {t4-t3:.2f} s')
 
 #Save dataframes
 #nuecc_3.to_pickle(f'{data_dir}/{savename}')
-nuecc_3.to_pickle(f'data/{savename}')
+nuecc_4.to_pickle(f'data/{savename}')
+nueccNp_4.to_pickle(f'data/{savename_Np}')
 #nue_4.to_pickle(f'{data_dir}/{savename_signal}')
-nue_4.to_pickle(f'data/{savename_signal}')
+nue_5.to_pickle(f'data/{savename_signal}')
 
 
 
